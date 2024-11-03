d5c53c7205f824abd36c4b36d64c6d46a035c22b
hritikk7 <kumarhritik158@gmail.com>
2024-11-04 01:16:55 +0530

    minor bug fix in timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,13 @@
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
-import { Pause, RefreshCw } from "lucide-react";
+import { Pause, Play, RefreshCw } from "lucide-react";
 
 const Timer: FC = () => {
-  const [minuteiInput, setMinuteInput] = useState<number>(0);
+  const [minuteInput, setMinuteInput] = useState<number>(0);
   const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
-  console.log("🚀 ~ isTimerRunning:", isTimerRunning);
   const [timeLeft, setTimeLeft] = useState<number>(0);
+  const [isTimerInitialized, setisTimerInitailized] = useState<boolean>(false);
 
   // console.log("🚀 ~ timeLeft:", timeLeft);÷
 
@@ -16,35 +16,39 @@ const Timer: FC = () => {
     setMinuteInput(minutes);
   };
 
-  const handleStart = () => {
-
-    setIsTimerRunning(true);
-    setTimeLeft(minuteiInput * 60);
+  useEffect(() => {
     if (isTimerRunning) {
       console.log("inside timer running ");
       const intervalId = setInterval(() => {
         console.log("called setinterval");
         setTimeLeft((prevTime) => {
-          console.log("🚀 ~ setTimeLeft ~ prevTime:", prevTime);
           if (prevTime <= 1) {
             setIsTimerRunning(false);
+            setisTimerInitailized(false);
             clearInterval(intervalId);
             return 0;
           }
           return prevTime - 1;
         });
       }, 1000);
+      return () => clearInterval(intervalId);
     }
+  }, [isTimerRunning, timeLeft]);
+
+  const handleStart = () => {
+    setIsTimerRunning(true);
+    setTimeLeft(minuteInput * 60);
+    setisTimerInitailized(true);
   };
 
   const handleReset = () => {
     setIsTimerRunning(false);
     setMinuteInput(0);
+    setisTimerInitailized(false);
     setTimeLeft(0);
   };
 
   const formattedTime = (seconds: number) => {
-    console.log(seconds);
     const minutes = String(Math.floor(seconds / 60));
     console.log("🚀 ~ formattedTime ~ minutes:", minutes);
     const remainingSeconds = String(Math.floor(seconds % 60));
@@ -57,8 +61,7 @@ const Timer: FC = () => {
         className="
       text-4xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl
       font-mono bg-gradient-to-r from-neutral-900 via-neutral-400 to-neutral-100
-      text-transparent bg-clip-text
-    "
+      text-transparent bg-clip-text"
       >
         {formattedTime(timeLeft)}
       </p>
@@ -66,12 +69,25 @@ const Timer: FC = () => {
         className="flex items-center justify-center md:text-lg text-muted-foreground
       text-sm  space-x-12"
       >
-        {isTimerRunning ? (
+        {isTimerInitialized ? (
           <div className=" flex gap-12">
-            <Button variant={"outline"} className="h-12 w-12 rounded-full ">
-              <Pause strokeWidth={1} />
+            <Button
+              variant={"outline"}
+              className="h-12 w-12 rounded-full "
+              onClick={() => setIsTimerRunning(!isTimerRunning)}
+            >
+              {isTimerRunning ? (
+                <Pause strokeWidth={1} />
+              ) : (
+                <Play strokeWidth={1} />
+              )}
             </Button>
-            <Button variant={"outline"} onClick={handleReset} className="h-12 w-12 rounded-full ">
+
+            <Button
+              variant={"outline"}
+              onClick={handleReset}
+              className="h-12 w-12 rounded-full "
+            >
               <RefreshCw strokeWidth={1} />
             </Button>
           </div>
@@ -86,7 +102,7 @@ const Timer: FC = () => {
             />
             <Button
               onClick={handleStart}
-              disabled={!minuteiInput}
+              disabled={!minuteInput}
               variant={"outline"}
             >
               {" "}
